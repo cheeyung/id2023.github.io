81eb2f801ba2d8afddf35432d513e31abefdc65e
cheeyung <thyecheeyung@gmail.com>
2023-09-17 02:17:57 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
-const currentTimeElement = document.getElementById('current-time');
-const bellSound = document.getElementById('bell-sound');
-// const bellInterval = 20 * 60 * 1000; // 20 minutes in milliseconds.
-// const bellInterval = 60 * 1000; // 1 minute in milliseconds.
+document.addEventListener('DOMContentLoaded', (event) => {
+    const currentTimeElement = document.getElementById('current-time');
+    const bellSound = new Audio('bell.mp3');
 
-// Function to update the current time on the web page
-function updateCurrentTime() {
+    function updateCurrentTime() {
+        const now = new Date();
+        const hours = now.getHours();
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const seconds = String(now.getSeconds()).padStart(2, '0');
+        const period = hours >= 12 ? 'PM' : 'AM';
+
+        let displayHours = hours % 12;
+        displayHours = displayHours ? displayHours : 12; // the hour '0' should be '12'
+        displayHours = String(displayHours).padStart(2, '0');
+
+        currentTimeElement.textContent = `${displayHours}:${minutes}:${seconds} ${period}`;
+    }
+
+    function chimeBell() {
+        bellSound.play();
+        setInterval(bellSound.play, 60 * 1000); // Play every minute after the initial chime
+    }
+
+    // Calculate milliseconds until the next minute
     const now = new Date();
-    const hours = now.getHours();
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
-    const period = hours >= 12 ? 'PM' : 'AM';
+    const millisecondsTillNextMinute = (60 - now.getSeconds()) * 1000 - now.getMilliseconds();
 
-    let displayHours = hours % 12;
-    displayHours = displayHours ? displayHours : 12; // the hour '0' should be '12'
-    displayHours = String(displayHours).padStart(2, '0');
+    // Update the time immediately upon page load
+    updateCurrentTime();
 
-    currentTimeElement.textContent = `${displayHours}:${minutes}:${seconds} ${period}`;
-}
+    // Then update every second
+    setInterval(updateCurrentTime, 1000);
 
-// Start updating the time immediately, and then set an interval to update every second
-updateCurrentTime();
-setInterval(updateCurrentTime, 1000);
-
-// Play the bell sound every 20 minutes
-setInterval(() => {
-    bellSound.play();
-}, bellInterval);
+    // Set the initial timeout to chime at the beginning of the next minute
+    setTimeout(chimeBell, millisecondsTillNextMinute);
+});
